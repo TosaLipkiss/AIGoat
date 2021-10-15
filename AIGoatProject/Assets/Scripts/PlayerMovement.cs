81003d8bc0e9ce63efda5dd191605891a4186098
TosaLipkiss <70565262+TosaLipkiss@users.noreply.github.com>
2021-10-15 11:21:00 +0200

    player completed

--- AIGoatProject/Assets/Scripts/PlayerMovement.cs
+++ AIGoatProject/Assets/Scripts/PlayerMovement.cs
@@ -8,13 +8,51 @@ public class PlayerMovement : MonoBehaviour
 
     public float speed = 12f;
 
+    [Header("Gravity")]
+    public float gravity;
+    public float currentGravity;
+    public float constantGravity;
+    public float maxGravity;
+
+    Vector3 gravityDirection;
+    Vector3 gravityMovement;
+
+    private void Awake()
+    {
+        gravityDirection = Vector3.down;
+    }
+
     void Update()
     {
+        CalculateGravity();
+
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
         Vector3 move = transform.right * x + transform.forward * z;
 
-        controller.Move(move * speed * Time.deltaTime);
+        controller.Move((move * speed * Time.deltaTime) + gravityMovement);
+    }
+
+    bool IsGrounded()
+    {
+        return controller.isGrounded;
+    }
+
+    void CalculateGravity()
+    {
+        if(IsGrounded())
+        {
+            currentGravity = constantGravity;
+        }
+        else
+        {
+            if(currentGravity > maxGravity)
+            {
+                currentGravity -= gravity * Time.deltaTime;
+            }
+        }
+
+        gravityMovement = gravityDirection * -currentGravity;
     }
 }
