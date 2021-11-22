e6bcb8b276f3a71fe369fc09f978440b36bfa832
TosaLipkiss <70565262+TosaLipkiss@users.noreply.github.com>
2021-11-22 16:30:32 +0100

    blendshape goat

--- AIGoatProject/Assets/Scripts/States/CharacterAgent.cs
+++ AIGoatProject/Assets/Scripts/States/CharacterAgent.cs
@@ -478,4 +478,5 @@ public class CharacterAgent : MonoBehaviour
     }
 
     #endregion
+
 }
---
+++ AIGoatProject/Assets/Scripts/States/GoatBlendShape.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoatBlendShape : MonoBehaviour
+{
+    SkinnedMeshRenderer skinnedMeshRenderer;
+    Mesh skinnedMesh;
+    int blendShapeCount;
+    float blendBlink = 0f;
+    float blendIdle = 0f;
+    float blendSpeed = 1f;
+    float blinkTimer = 0f;
+    int randomBlinkTimerLimit = 1;
+    bool blinkedFInished = false;
+
+    void Start()
+    {
+        skinnedMeshRenderer = GetComponent<SkinnedMeshRenderer>();
+        skinnedMesh = skinnedMeshRenderer.sharedMesh;
+
+        blendShapeCount = skinnedMesh.blendShapeCount;
+    }
+
+    void Update()
+    {
+        blinkTimer += Time.deltaTime;
+        BlinkBlend();
+    }
+
+    public void BlinkBlend()
+    {
+        if(blinkTimer > randomBlinkTimerLimit)
+        {
+            if (blinkedFInished == false && blendBlink < 100f)
+            {
+                skinnedMeshRenderer.SetBlendShapeWeight(0, blendBlink);
+                blendBlink += blendSpeed;
+            }
+            else
+            {
+                blinkedFInished = true;
+                blinkTimer = 0f;
+            }
+        }
+
+        if (blinkedFInished == true && blendBlink > 0f)
+        {
+            skinnedMeshRenderer.SetBlendShapeWeight(0, blendBlink);
+            blendBlink -= blendSpeed;
+
+            if(blendBlink <= 0f)
+            {
+                blinkedFInished = false;
+                randomBlinkTimerLimit = Random.Range(1,7);
+            }
+        }
+    }
+}
